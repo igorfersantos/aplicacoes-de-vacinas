01278533ab5955f9cb6bf1de8974a5f99999328e
Igor Fernandes dos Santos <igorfercontato@gmail.com>
2021-03-07 22:27:37 -0300

    init

---
+++ src/main/java/com/igorfersantos/aplicacoesdevacinas/AplicacoesdevacinasApplication.java
@@ -0,0 +1,13 @@
+package com.igorfersantos.aplicacoesdevacinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacoesdevacinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacoesdevacinasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorfersantos/aplicacoesdevacinas/AplicacoesdevacinasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorfersantos.aplicacoesdevacinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacoesdevacinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
